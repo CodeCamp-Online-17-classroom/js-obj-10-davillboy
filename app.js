459cc00efcdd477be25335a875b8b8f613b64f44
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 16:53:56 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // write code here
+<script>
+        let fruitBasket = {};
+        while (true) {
+            let fruit = prompt("Enter fruit name (or 'stop' to finish):");
+          
+        
+            if (fruit.toLowerCase() === "stop") {
+              break;
+            }
+          
+            let quantity = +prompt(`Enter quantity of ${fruit}:`);
+          
+          
+            if (quantity > 1) {
+              fruit += "s"; // เพิ่ม s หลังชื่อ fruit
+            }
+          
+           
+            fruitBasket[fruit] = quantity;
+          }
+          
+          console.log(fruitBasket);
+          
+    </script>
